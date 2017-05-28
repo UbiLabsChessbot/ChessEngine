dac7bf07202d106afc0c74ed8c9e977d6c4fafe3
lucachen <31401089@stu.zucc.edu.cn>
2017-05-28 16:42:22 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,184 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import abort
+from flask import request
+from sunfish import Position
+from sunfish import Searcher
+import re, sys, time
+import json
+
+TABLE_SIZE = 1e8
+
+# Mate value must be greater than 8*queen + 2*(rook+knight+bishop)
+# King value is set to twice this value such that if the opponent is
+# 8 queens up, but we got the king, we still exceed MATE_VALUE.
+# When a MATE is detected, we'll set the score to MATE_UPPER - plies to get there
+# E.g. Mate in 3 will be MATE_UPPER - 6
+MATE_LOWER = 60000 - 8*2700
+MATE_UPPER = 60000 + 8*2700
+
+QS_LIMIT = 150
+EVAL_ROUGHNESS = 20
+
+# Our board is represented as a 120 character string. The padding allows for
+# fast detection of moves that don't stay within the board.
+A1, H1, A8, H8 = 91, 98, 21, 28
+initial = (
+    '         \n'  #   0 -  9
+    '         \n'  #  10 - 19
+    ' rnbqkbnr\n'  #  20 - 29
+    ' pppppppp\n'  #  30 - 39
+    ' ........\n'  #  40 - 49
+    ' ........\n'  #  50 - 59
+    ' ........\n'  #  60 - 69
+    ' ........\n'  #  70 - 79
+    ' PPPPPPPP\n'  #  80 - 89
+    ' RNBQKBNR\n'  #  90 - 99
+    '         \n'  # 100 -109
+    '         \n'  # 110 -119
+)
+
+###############################################################################
+# Move and evaluation tables
+###############################################################################
+
+N, E, S, W = -10, 1, 10, -1
+directions = {
+    'P': (N, N+N, N+W, N+E),
+    'N': (N+N+E, E+N+E, E+S+E, S+S+E, S+S+W, W+S+W, W+N+W, N+N+W),
+    'B': (N+E, S+E, S+W, N+W),
+    'R': (N, E, S, W),
+    'Q': (N, E, S, W, N+E, S+E, S+W, N+W),
+    'K': (N, E, S, W, N+E, S+E, S+W, N+W)
+}
+
+pst = {
+    'P': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 198, 198, 198, 198, 198, 198, 198, 198, 0,
+        0, 178, 198, 198, 198, 198, 198, 198, 178, 0,
+        0, 178, 198, 198, 198, 198, 198, 198, 178, 0,
+        0, 178, 198, 208, 218, 218, 208, 198, 178, 0,
+        0, 178, 198, 218, 238, 238, 218, 198, 178, 0,
+        0, 178, 198, 208, 218, 218, 208, 198, 178, 0,
+        0, 178, 198, 198, 198, 198, 198, 198, 178, 0,
+        0, 198, 198, 198, 198, 198, 198, 198, 198, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    'B': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 797, 824, 817, 808, 808, 817, 824, 797, 0,
+        0, 814, 841, 834, 825, 825, 834, 841, 814, 0,
+        0, 818, 845, 838, 829, 829, 838, 845, 818, 0,
+        0, 824, 851, 844, 835, 835, 844, 851, 824, 0,
+        0, 827, 854, 847, 838, 838, 847, 854, 827, 0,
+        0, 826, 853, 846, 837, 837, 846, 853, 826, 0,
+        0, 817, 844, 837, 828, 828, 837, 844, 817, 0,
+        0, 792, 819, 812, 803, 803, 812, 819, 792, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    'N': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 627, 762, 786, 798, 798, 786, 762, 627, 0,
+        0, 763, 798, 822, 834, 834, 822, 798, 763, 0,
+        0, 817, 852, 876, 888, 888, 876, 852, 817, 0,
+        0, 797, 832, 856, 868, 868, 856, 832, 797, 0,
+        0, 799, 834, 858, 870, 870, 858, 834, 799, 0,
+        0, 758, 793, 817, 829, 829, 817, 793, 758, 0,
+        0, 739, 774, 798, 810, 810, 798, 774, 739, 0,
+        0, 683, 718, 742, 754, 754, 742, 718, 683, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    'R': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 1258, 1263, 1268, 1272, 1272, 1268, 1263, 1258, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    'Q': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 2529, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    'K': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 60098, 60132, 60073, 60025, 60025, 60073, 60132, 60098, 0,
+        0, 60119, 60153, 60094, 60046, 60046, 60094, 60153, 60119, 0,
+        0, 60146, 60180, 60121, 60073, 60073, 60121, 60180, 60146, 0,
+        0, 60173, 60207, 60148, 60100, 60100, 60148, 60207, 60173, 0,
+        0, 60196, 60230, 60171, 60123, 60123, 60171, 60230, 60196, 0,
+        0, 60224, 60258, 60199, 60151, 60151, 60199, 60258, 60224, 0,
+        0, 60287, 60321, 60262, 60214, 60214, 60262, 60321, 60287, 0,
+        0, 60298, 60332, 60273, 60225, 60225, 60273, 60332, 60298, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+}
+def parse(c):
+    fil, rank = ord(c[0]) - ord('a'), int(c[1]) - 1
+    return A1 + fil - 10*rank
+
+
+def render(i):
+    rank, fil = divmod(i - A1, 10)
+    return chr(fil + ord('a')) + str(-rank + 1)
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def get():
+     global pos
+     global searcher
+     message = request.args.get('move')
+     data = {'code':1,'message':123}
+     date = json.dumps(data)
+     result=json.loads(date)
+     if message == '123':
+        pos = Position(initial, 0, (True,True), (True,True), 0, 0)
+        searcher = Searcher()
+        result['code']=5
+        result['message']="play again"
+        return jsonify(result)
+     else:
+        match = re.match('([a-h][1-8])'*2, message)
+        if match:
+            move = parse(match.group(1)), parse(match.group(2))
+        else:
+            print("Please enter a move like g8f6")
+            result['code']=0;
+            result['message']="Please enter a move like g8f6"
+            return jsonify(result)
+        if move not in pos.gen_moves():
+            result['code']=0;
+            result['message']="Please enter a move like g8f6"
+            return jsonify(result)
+        pos = pos.move(move)
+        if pos.score <= -MATE_LOWER:
+            print("You won")
+            result['code']=2
+            result['message']="You won"
+            return jsonify(result)
+        move, score = searcher.search(pos, secs=2)
+        pos = pos.move(move)
+        if pos.score <= -MATE_LOWER:
+            result['code']=3
+            result['message']=render(119-move[0]) + render(119-move[1])
+            return jsonify(result)
+        print("My move:", render(119-move[0]) + render(119-move[1]))
+        result['message']=render(119-move[0]) + render(119-move[1])
+        result['code']=1
+        return jsonify(result)
+if __name__ == '__main__':
+    pos = Position(initial, 0, (True,True), (True,True), 0, 0)
+    searcher = Searcher()
+    app.run(host='0.0.0.0',port=8080)
